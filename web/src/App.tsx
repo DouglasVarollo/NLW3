3ceba985da6f7b3fc6342ca2025d1497aac0b797
Douglas Varollo <dcvarollo@gmail.com>
2020-10-12 17:46:30 -0300

    add global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import "./styles/global.css";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello World</h1>
+		</div>
+	);
 }
 
 export default App;
