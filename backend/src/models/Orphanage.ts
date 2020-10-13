a9d1885e92ab3478c1b15c5232eeb0333a6b4214
Douglas Varollo <dcvarollo@gmail.com>
2020-10-13 15:51:34 -0300

    add orphanage model

---
+++ backend/src/models/Orphanage.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("orphanages")
+class Orphanage {
+	@PrimaryGeneratedColumn("increment")
+	id: number;
+
+	@Column()
+	name: string;
+
+	@Column()
+	latitude: number;
+
+	@Column()
+	longitude: number;
+
+	@Column()
+	about: string;
+
+	@Column()
+	instructions: string;
+
+	@Column()
+	opening_hours: number;
+
+	@Column()
+	open_on_weekengs: boolean;
+}
+
+export default Orphanage;
