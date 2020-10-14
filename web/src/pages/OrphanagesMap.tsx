6c0af016616b6d29333b2b93827a3c17b15ed0a9
Douglas Varollo <dcvarollo@gmail.com>
2020-10-14 15:57:48 -0300

    add abstract components

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { FiPlus } from "react-icons/fi";
-import { Map, TileLayer } from "react-leaflet";
+import { FiArrowRight, FiPlus } from "react-icons/fi";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Link } from "react-router-dom";
-import "leaflet/dist/leaflet.css";
+
+import mapIcon from "../utils/mapIcon";
 
 import "../styles/pages/orphanages-map.css";
-
 import mapMarkerImg from "../images/map-marker.svg";
 
 function OrphanagesMap() {
@@ -38,9 +38,23 @@ function OrphanagesMap() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+
+        <Marker icon={mapIcon} position={[-27.2092052, -49.6401092]}>
+          <Popup
+            className="map-popup"
+            closeButton={false}
+            minWidth={240}
+            maxHeight={240}
+          >
+            Lar das meninas
+            <Link to="/orphanages/1">
+              <FiArrowRight color="#fff" size={20} />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
-      <Link className="create-orphanage" to="/">
+      <Link className="create-orphanage" to="/orphanages/create">
         <FiPlus color="#fff" size={32} />
       </Link>
     </div>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,16 +3,20 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Landing from "./pages/Landing";
 import OrphanagesMap from "./pages/OrphanagesMap";
+import Orphanage from "./pages/Orphanage";
+import CreateOrphanage from "./pages/CreateOrphanage";
 
 function Routes() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route component={Landing} exact path="/" />
-        <Route component={OrphanagesMap} path="/app" />
-      </Switch>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route component={Landing} exact path="/" />
+				<Route component={OrphanagesMap} path="/app" />
+				<Route component={CreateOrphanage} path="/orphanages/create" />
+				<Route component={Orphanage} path="/orphanages/:id" />
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default Routes;
